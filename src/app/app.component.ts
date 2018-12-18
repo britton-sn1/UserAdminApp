72b848c535ae01dbc7624b529df103e0827e3e08
snowjest <git@snowjest.plus.com>
2018-12-18 11:41:13 +0000

    Added a list of users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { UserComponent } from './model/user/user.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UserAdminApp';
+    users: UserComponent[] = [{ username: 'admin', id: 201 },{ username: 'neil', id: 202 },{ username: 'eric', id: 203 }];
+
+    title = 'UserAdminApp';
 }
